653c2a1817b6d51ea78a2c598d7ea793b9847eaf
LouisePrd <louise.peridy22@gmail.com>
2025-09-30 22:35:36 +0200

    adjust control Panel parameters

--- TPs/TP_Lwiz/src/App.jsx
+++ TPs/TP_Lwiz/src/App.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import Scene from "./components/Scene";
 import ControlPanel from "./components/ControlPanel";
 
 export default function App() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [speed, setSpeed] = useState(500);
+  const [gridSize, setGridSize] = useState(10);
+
   return (
     <div className="layout">
-      <Scene />
-      <ControlPanel />
+      <Scene isRunning={isRunning} speed={speed} gridSize={gridSize} />
+      <ControlPanel
+        isRunning={isRunning}
+        setIsRunning={setIsRunning}
+        speed={speed}
+        setSpeed={setSpeed}
+        gridSize={gridSize}
+        setGridSize={setGridSize}
+      />
     </div>
   );
 }
--- TPs/TP_Lwiz/src/components/ControlPanel.jsx
+++ TPs/TP_Lwiz/src/components/ControlPanel.jsx
@@ -1,26 +1,46 @@
-
-
-export default function ControlPanel() {
+export default function ControlPanel({
+  isRunning,
+  setIsRunning,
+  speed,
+  setSpeed,
+  gridSize,
+  setGridSize,
+}) {
   return (
     <div className="panel" style={{ color: "#8d8d8d" }}>
-      <button id="startStopBtn">Start/Stop</button>
-      <button id="clearBtn">Clear</button>
+      <button onClick={() => setIsRunning(!isRunning)}>
+        {isRunning ? "Stop" : "Start"}
+      </button>
+      <button
+        onClick={() => {
+          window.dispatchEvent(new Event("clearGrid"));
+          setIsRunning(false);
+        }}
+      >
+        Clear
+      </button>
+
       <br />
       <label htmlFor="speedRange">Speed:</label>
       <input
         type="range"
-        id="speedRange"
-        min="100"
+        min="50"
         max="2000"
-        step="100"
-        defaultValue="1000"
+        step="50"
+        value={speed}
+        onChange={(e) => setSpeed(Number(e.target.value))}
       />
+
       <br />
       <label htmlFor="sizeSelect">Grid Size:</label>
-      <select id="sizeSelect">
+      <select
+        id="sizeSelect"
+        value={gridSize}
+        onChange={(e) => setGridSize(Number(e.target.value))}
+      >
+        <option value="10">10x10</option>
         <option value="20">20x20</option>
         <option value="50">50x50</option>
-        <option value="100">100x100</option>
       </select>
     </div>
   );
