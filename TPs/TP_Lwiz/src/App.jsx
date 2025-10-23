06f13333d5734776dbeb6c9577cfaaf327cd3a33
LouisePrd <louise.peridy22@gmail.com>
2025-10-23 21:41:30 +0200

    Mandelbrot TP

--- TPs/TP_Lwiz/src/App.jsx
+++ TPs/TP_Lwiz/src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
-import "./App.css";
-import Scene from "./components/Scene";
-import ControlPanel from "./components/ControlPanel";
+// import GameOfLife from "./components/GameOfLife/GameOfLife";
+// import ControlPanel from "./components/GameOfLife/ControlPanel";
+import MandelbrotTerrain from "./components/Mandelbrot";
 
 export default function App() {
   const [isRunning, setIsRunning] = useState(false);
   const [speed, setSpeed] = useState(500);
   const [gridSize, setGridSize] = useState(10);
 
+  // height & width for MandelbrotTerrain based on window size
+  const mandelbrotWidth = window.innerWidth * 0.9;
+  const mandelbrotHeight = window.innerHeight * 0.9;
+
   return (
     <div className="layout">
-      <Scene isRunning={isRunning} speed={speed} gridSize={gridSize} />
+      {/* <Scene isRunning={isRunning} speed={speed} gridSize={gridSize} /> 
       <ControlPanel
         isRunning={isRunning}
         setIsRunning={setIsRunning}
@@ -18,7 +22,10 @@ export default function App() {
         setSpeed={setSpeed}
         gridSize={gridSize}
         setGridSize={setGridSize}
-      />
+      />*/}
+
+      <MandelbrotTerrain width={mandelbrotWidth} height={mandelbrotHeight} />
+
     </div>
   );
 }
