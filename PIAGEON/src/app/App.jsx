187c5c4c7fb5dc30122ef4cdef9ba0a0e7cd281e
LouisePrd <louise.peridy22@gmail.com>
2026-01-30 01:53:59 +0100

    new font

--- PIAGEON/src/app/App.jsx
+++ PIAGEON/src/app/App.jsx
@@ -1,10 +1,16 @@
+import { useState, useEffect } from "react";
 import { SceneCanvas } from "../features/world/components/SceneCanvas";
 import { PigeonHud } from "../features/pigeons/components/PigeonHud.jsx";
 import { PigeonCard } from "../features/pigeons/components/PigeonCard.jsx";
+import { LoadingScreen } from "./Loading/LoadingScreen.jsx";
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
   return (
     <div className="app-root">
+      {!isReady && <LoadingScreen onFinished={() => setIsReady(true)} />}
+
       <header className="ui-header">
         <h1>Piageon – Île aux Pigeons</h1>
       </header>
@@ -17,7 +23,7 @@ export default function App() {
 
       <footer className="ui-footer">
         <span>
-          Contrôle de la direction de la caméra avec les <strong>Flèches</strong> et <strong>ZQSD</strong> & <strong>Space / Shift</strong> pour monter et descendre — Maintenir <strong>F</strong> pour accélérer le temps
+          Contrôle de la direction de la caméra avec les <strong>Flèches</strong> et <strong>ZQSD</strong>...
         </span>
       </footer>
     </div>
